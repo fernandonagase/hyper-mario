fa8cd6bde1c6f22603d33d5f7fd301b04cbbc404
Fernando Nagase <nagase.fernando@gmail.com>
2020-11-19 12:42:09 -0300

    refactor: move movement logic from MarioBehaviour

---
+++ Assets/Scripts/Controllers/MarioController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MarioController : CharacterController
+{
+    private const float WalkSpeed = 5f;
+    private const float RunFactor = 1.6f;
+    private const float JoystickDeadzone = 0.15f;
+
+    private float _movement;
+    private bool _isRunning;
+
+    [SerializeField]
+    private VirtualButton _jumpButton = null;
+    [SerializeField]
+    private VirtualJoystick _joystick = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movable = gameObject.GetComponent<MarioBehaviour>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ((MarioBehaviour)movable).Crouch(Input.GetKey(KeyCode.S));
+
+        _movement = Input.GetAxisRaw("Horizontal");
+        _movement = _movement != 0 ? _movement : _joystick.GetHorizontal();
+        _movement = Mathf.Abs(_movement) > JoystickDeadzone ? _movement : 0.0f;
+        _isRunning = _movement != 0 && Input.GetKey(KeyCode.LeftShift);
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = _movement * WalkSpeed;
+        if (_isRunning) speed *= RunFactor;
+        movable.Move(Vector2.right * speed);
+    }
+}
--- Assets/Scripts/MarioBehaviour.cs
+++ Assets/Scripts/MarioBehaviour.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class MarioBehaviour : MonoBehaviour
+public class MarioBehaviour : MonoBehaviour, IMovable
 {
     [SerializeField]
     private AudioSource _sfxSource;
@@ -12,14 +12,8 @@ public class MarioBehaviour : MonoBehaviour
     private float jumpImpulse = 10f;
     [SerializeField]
     private float runFactor = 1.6f;
-    [SerializeField]
-    private VirtualButton _jumpButton = null;
-    [SerializeField]
-    private VirtualJoystick _joystick = null;
     private Rigidbody2D rb2d;
     private Animator animCtrl;
-    private float horizontal = 0f;
-    private float _deadzone = 0.15f;
     private bool isGrounded = false;
     private bool _isCrouched = false;
 
@@ -33,43 +27,18 @@ public class MarioBehaviour : MonoBehaviour
     {
         CheckGround();
 
-        horizontal = Input.GetAxisRaw("Horizontal");
-        if (horizontal != 0)
-        {
-            horizontal = _isCrouched ? 0 : horizontal;
-        }
-        else
-        {
-            horizontal = _isCrouched ? 0 : _joystick.GetHorizontal();
-            horizontal = Mathf.Abs(horizontal) > _deadzone ? horizontal : 0.0f;
-        }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (!isGrounded) return;
             Jump();
         }
-        else if (isGrounded && _jumpButton.DownEvent)
-        {
-            Jump();
-        }
-
-        Crouch(Input.GetKey(KeyCode.S));
+        //else if (isGrounded && _jumpButton.DownEvent)
+        //{
+        //    Jump();
+        //}
 
         UpdateAnimation();
     }
-
-    void FixedUpdate()
-    {
-        float horizontalSpeed = horizontal * speedMax;
-        bool isRunning = horizontalSpeed != 0 && Input.GetKey(KeyCode.LeftShift);
-        if (isRunning)
-        {
-            horizontalSpeed *= runFactor;
-        }
-        Move(Vector2.right * horizontalSpeed);
-    }
-
 
     void UpdateAnimation()
     {
@@ -89,27 +58,18 @@ public class MarioBehaviour : MonoBehaviour
         Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red);
     }
 
-
-
-
-
-
-
-
-
     public void Move(Vector3 velocity)
     {
         animCtrl.SetFloat("speed", Mathf.Abs(velocity.x));
+        // Mover responsabilidade de animação
+        animCtrl.SetBool("isRunning", Mathf.Abs(velocity.x) > speedMax);
+
+        if (_isCrouched) return;
 
         if (velocity.x != 0)
         {
             animCtrl.SetFloat("direction", velocity.x);
         }
-
-        bool isRunning = Mathf.Abs(velocity.x) > speedMax;
-        // Mover responsabilidade de animação
-        animCtrl.SetBool("isRunning", isRunning);
-
         transform.position += velocity * Time.deltaTime;
     }
 
